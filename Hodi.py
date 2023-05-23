4a911e2c94ddbff9bcea701cc77fa6e18512a6d5
avi-ar <83911910+avi-ar@users.noreply.github.com>
2023-05-23 09:35:07 +0300

    Update Hodi.py
    added a few code lines

--- Hodi.py
+++ Hodi.py
@@ -20,3 +20,15 @@ def reciver1():
             if word == word1: print("gj")
             break
     except:print("ERR")
+
+def biggerlist():
+    list1 = [1,2,4,17,32]
+    list2 = [45,67,12,-40,78]
+
+    list1sum = sum(list1)
+    list2sum = sum(list2)
+    try:
+        if list1sum> list2sum:
+        print("list 1 is bigger")
+        else: print("list2 is the bigger one :) ")
+    except:print("err")
